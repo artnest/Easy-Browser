42a19d5951f49d84ac0c5f88d69d37880d52bbfd
artnest <artnest789@gmail.com>
2020-03-16 00:52:44 +0300

    Add WebKit Add default web site load Add UINavigationController

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    private var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
-
 }
-
